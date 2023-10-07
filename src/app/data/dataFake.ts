188f5dcffcd1816045e1ec17ad904e8875f674d9
Luiz Santos <lfp.santos543@gmail.com>
2023-10-07 20:24:45 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo Homem de Ferro',
+    description: 'Marvel anuncia novo homem de ferro.',
+    photoCover:
+      'https://assetsio.reedpopcdn.com/iron-man_aJ1y3LP.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp',
+  },
+  {
+    id: '2',
+    title: 'Novas Séries Anunciada no Disney+',
+    description:
+      'Disney anuncia as novas séries que estão chegando no catálogo do streamer.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+];
